24e9f2560209312c1a44081aefce0df38cf5df62
dinar wijaksono <dinarwijaksono11@gmail.com>
2024-07-06 21:45:25 +0700

    Feature: setup database migrations

---
+++ database/migrations/2024_07_06_142946_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('category_id');
+            $table->foreignId('period_id');
+            $table->string('code', 10);
+            $table->bigInteger('date');
+            $table->string('description', 100);
+            $table->integer('income');
+            $table->integer('spending');
+            $table->bigInteger('created_at');
+            $table->bigInteger('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
