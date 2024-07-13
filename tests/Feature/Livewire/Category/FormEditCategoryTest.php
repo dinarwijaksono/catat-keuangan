eacef545cacc01f31574f727fae6b6bf7a291067
dinar wijaksono <dinarwijaksono11@gmail.com>
2024-07-13 08:55:14 +0700

    Test: menambahkan test FormEditCategoryTest

--- tests/Feature/Livewire/Category/FormEditCategoryTest.php
+++ tests/Feature/Livewire/Category/FormEditCategoryTest.php
@@ -3,6 +3,10 @@
 namespace Tests\Feature\Livewire\Category;
 
 use App\Livewire\Category\FormEditCategory;
+use App\Models\Category;
+use App\Models\User;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\UserRegisterSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -10,10 +14,44 @@ use Tests\TestCase;
 
 class FormEditCategoryTest extends TestCase
 {
-    /** @test */
-    public function renders_successfully()
+    public $user;
+    public $category;
+
+    public function setUp(): void
     {
-        Livewire::test(FormEditCategory::class)
+        parent::setUp();
+
+        $this->seed(UserRegisterSeeder::class);
+        $this->user = User::select('*')->first();
+        $this->actingAs($this->user);
+
+        $this->seed(CategorySeeder::class);
+        $this->category = Category::select('*')->first();
+    }
+
+    public function test_renders_successfully()
+    {
+        Livewire::test(FormEditCategory::class, ['code' => $this->category->code])
             ->assertStatus(200);
+    }
+
+    public function test_do_update_failed_validate_is_wrong()
+    {
+        Livewire::test(FormEditCategory::class, ['code' => $this->category->code])
+            ->set('categoryName', '')
+            ->call('doUpdate')
+            ->assertHasErrors(['categoryName' => 'required']);
+    }
+
+    public function test_do_update_success()
+    {
+        Livewire::test(FormEditCategory::class, ['code' => $this->category->code])
+            ->set('categoryName', 'example category name')
+            ->call('doUpdate');
+
+        $this->assertDatabaseHas('categories', [
+            'name' => 'example category name',
+            'code' => $this->category->code
+        ]);
     }
 }
