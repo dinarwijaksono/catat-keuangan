daf982774215c1b0320ad839f58d602a9936764b
dinar wijaksono <dinarwijaksono11@gmail.com>
2024-10-02 23:56:13 +0700

    Feature: (repository) membuat fitur create start date

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Models\StartDate;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
@@ -25,6 +26,27 @@ class UserRepository
         } catch (\Throwable $th) {
             Log::error("insert user to database failed", [
                 'email' => $email,
+                'message' => $th->getMessage()
+            ]);
+        }
+    }
+
+    public function setStartDate(User $user, int $startDate): void
+    {
+        try {
+            StartDate::insert([
+                'user_id' => $user->id,
+                'date' => $startDate,
+                'created_at' => round(microtime(true) * 1000),
+                'updated_at' => round(microtime(true) * 1000),
+            ]);
+
+            Log::info('insert start date to database success', [
+                'email' => $user->email,
+            ]);
+        } catch (\Throwable $th) {
+            Log::error('insert start date to database failed', [
+                'email' => $user->email,
                 'message' => $th->getMessage()
             ]);
         }
--- tests/Feature/Repository/UserRepositoryTest.php
+++ tests/Feature/Repository/UserRepositoryTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature\Repository;
 
+use App\Models\User;
 use App\Repository\UserRepository;
+use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -33,5 +35,19 @@ class UserRepositoryTest extends TestCase
 
         $this->assertEquals($response->name, $name);
         $this->assertEquals($response->email, $email);
+    }
+
+    public function test_set_start_date()
+    {
+        $this->seed(UserSeeder::class);
+
+        $user = User::select('*')->first();
+
+        $this->userRepository->setStartDate($user, 10);
+
+        $this->assertDatabaseHas('start_dates', [
+            'user_id' => $user->id,
+            'date' => 10
+        ]);
     }
 }
