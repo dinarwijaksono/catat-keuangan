ca740fd463f0c9906b278ed742135f4580c1c148
dinar wijaksono <dinarwijaksono11@gmail.com>
2024-11-21 06:56:33 +0700

    Redesain: komponen box-total-category-by-period

--- resources/views/livewire/report/box-total-category-by-period.blade.php
+++ resources/views/livewire/report/box-total-category-by-period.blade.php
@@ -26,26 +26,32 @@
                 <thead>
                     <tr class="bg-green-500 text-white uppercase">
                         <th class="py-1 px-2">Kategori</th>
-                        <th class="py-1 px-2">Nilai</th>
+                        <th class="py-1 px-2">Pemasukan</th>
+                        <th class="py-1 px-2">Pengeluaran</th>
                     </tr>
                 </thead>
 
                 @php
                     $totalIncome = 0;
+                    $totalSpending = 0;
                 @endphp
 
                 @foreach ($transaction as $key)
-                    @if ($key->total_income != 0)
-                        @php
-                            $totalIncome += $key->total_income;
-                        @endphp
+                    @php
+                        $totalIncome += $key->total_income;
+                        $totalSpending += $key->total_spending;
+                    @endphp
 
-                        <tr class="border-b">
-                            <td class="py-3 px-4 border-b border-gray-200">{{ $key->category_name }}</td>
-                            <td class="py-3 px-4 border-b border-gray-200 text-right font-bold text-green-500">
-                                {{ number_format($key->total_income, 0) }}</td>
-                        </tr>
-                    @endif
+                    <tr class="border-b">
+                        <td class="py-3 px-4 border-b border-gray-200">
+                            <a href="/category/detail/{{ $key->category_code }}"
+                                class="text-blue-500 hover:underline">{{ $key->category_name }}</a>
+                        </td>
+                        <td class="py-3 px-4 border-b border-gray-200 text-right text-green-500">
+                            {{ number_format($key->total_income, 0) }}</td>
+                        <td class="py-3 px-4 border-b border-gray-200 text-right text-red-500">
+                            {{ number_format($key->total_spending, 0) }}</td>
+                    </tr>
                 @endforeach
 
                 <tfoot>
@@ -53,49 +59,12 @@
                         <td class="py-3 px-4 border-b border-gray-200">total</td>
                         <td class="py-3 px-4 border-b border-gray-200 text-right">
                             {{ number_format($totalIncome, 0) }}</td>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
-
-        <div class="mb-4">
-            <h3 class="font-bold mx-4 mt-4 mb-2 underline">Kategori pengeluaran</h3>
-
-            <table class="min-w-full bg-white">
-                <thead>
-                    <tr class="bg-red-500 text-white uppercase">
-                        <th class="py-1 px-2">Kategori</th>
-                        <th class="py-1 px-2">Nilai</th>
-                    </tr>
-                </thead>
-
-                @php
-                    $totalSpending = 0;
-                @endphp
-
-                @foreach ($transaction as $key)
-                    @if ($key->total_spending != 0)
-                        @php
-                            $totalSpending += $key->total_spending;
-                        @endphp
-
-                        <tr class="border-b">
-                            <td class="py-3 px-4 border-b border-gray-200">{{ $key->category_name }}</td>
-                            <td class="py-3 px-4 border-b border-gray-200 text-right font-bold text-red-500">
-                                {{ number_format($key->total_spending, 0) }}</td>
-                        </tr>
-                    @endif
-                @endforeach
-
-                <tfoot>
-                    <tr class="bg-red-300 font-bold">
-                        <td class="py-3 px-4 border-b border-gray-200">total</td>
-                        <td class="py-3 px-4 border-b border-gray-200 text-right">
+                        <td class="py-3 px-4 border-b border-gray-200 text-right text-red-500">
                             {{ number_format($totalSpending, 0) }}</td>
                     </tr>
+
                 </tfoot>
             </table>
-
         </div>
 
     @endif
